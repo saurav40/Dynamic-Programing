e76f6e78631f0c69bfdf955c47467bc4c4f4efd4
saurav40 <42322664+saurav40@users.noreply.github.com>
2022-03-10 04:34:45 +0530

    Update House_robber.java

--- House_robber/House_robber.java
+++ House_robber/House_robber.java
@@ -12,3 +12,23 @@ int dp[]=new int[nums.length];
         }
         return dp[nums.length-1];
     }
+/*
+
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        if(n==1)
+            return nums[0];
+        
+        int prev2=nums[0];
+        int prev1=Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++)
+        {
+            int curr=Math.max(prev2+nums[i],prev1);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+}
+*/
