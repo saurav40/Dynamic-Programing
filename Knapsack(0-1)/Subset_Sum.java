86990233509f454ccce7346733b5efe73cb98a54
saurav40 <42322664+saurav40@users.noreply.github.com>
2022-03-11 03:06:44 +0530

    Update Subset_Sum.java

--- Knapsack(0-1)/Subset_Sum.java
+++ Knapsack(0-1)/Subset_Sum.java
@@ -26,3 +26,40 @@
            }
            return dp[N][sum];
     }
+//----------------------------------------Memoization------------
+/*
+class Solution{
+
+    int dp[][]=new int[101][100001];
+    Boolean isSubsetSum(int N, int arr[], int sum){
+        // code here
+        for(int a[]:dp)
+        {
+            Arrays.fill(a,-1);
+        }
+        
+        if(ss(N,arr,sum)!=1)
+            return false;
+        return true;
+         
+    }
+   public int ss(int n,int [] arr,int sum)
+   {
+       if(sum==0)
+         return 1;
+       if(n<=0)
+          return 0;
+        if(dp[n][sum]!=-1)
+           return dp[n][sum];
+        if(arr[n-1]<=sum)
+        {
+            if(ss(n-1,arr,sum-arr[n-1])!=0 || ss(n-1,arr,sum)!=0)
+               dp[n][sum]=1;
+            else
+              dp[n][sum]=0;
+        }
+        else
+          dp[n][sum]=ss(n-1,arr,sum);
+    return dp[n][sum];
+   }
+}*/
